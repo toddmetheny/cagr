184e54b282a42fdeb8cd8e39a215c3d629c67ae6
Todd Metheny <todd.metheny@gmail.com>
2015-01-08 17:52:48 -0500

    added some jquery

--- public/cagr.js
+++ public/cagr.js
@@ -6,6 +6,6 @@ $(document).ready(function(){
 		var cagr = (Math.pow((ending / starting), (1 / periods)) - 1);
 		console.log(Math.round(cagr * 100));
 		var to_percent = "\n" + Math.round(cagr * 100) + "%" + "\n";
-		$(".all").append(to_percent);
+		$(".all").append("<p>" + to_percent + "</p>").first('p').addClass("rate");
 	});
 });
